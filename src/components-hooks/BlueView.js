1997b8a021863daa18634734f26d0bcca0638b46
Jeremy Tupper <jeremytupper1@gmail.com>
2020-09-16 17:36:30 -0400

    Finish the functional version of day one views

--- src/components-hooks/BlueView.js
+++ src/components-hooks/BlueView.js
@@ -1,19 +1,89 @@
-import React from 'react';
+import React, { useState, useMemo, useRef, useLayoutEffect, useEffect } from 'react';
 
 function BlueView({ numList }) {
+  const [filter, setFilter] = useState(null);
+  const listRef = useRef(null);
+
+  const filteredList = useMemo(
+    () =>
+      numList.filter(
+        (num) =>
+          (filter === 'even' && num % 2 === 0) || (filter === 'odd' && num % 2) || filter === null
+      ),
+    [numList, filter]
+  );
+
+  const prevScrollHeight = useRef(null);
+  const prevScrollTop = useRef(null);
+  const componentJustMounted = useRef(true);
+  useLayoutEffect(() => {
+    if (!componentJustMounted.current) {
+      listRef.current.scrollTop =
+        listRef.current.scrollHeight - (prevScrollHeight.current - prevScrollTop.current);
+    } else {
+      // scroll to bottom when component is first mounted
+      listRef.current.scrollTop = listRef.current.scrollHeight - listRef.current.offsetHeight;
+      componentJustMounted.current = false;
+    }
+    // capture the old scroll height and scroll top to use next time the numList changes
+    prevScrollHeight.current = listRef.current.scrollHeight;
+    prevScrollTop.current = listRef.current.scrollTop;
+  }, [filteredList]);
+
+  // listen for changes to scroll top so we can use it next time numList changes
+  useEffect(() => {
+    const currListRef = listRef.current;
+    const handleScroll = () => {
+      prevScrollHeight.current = listRef.current.scrollHeight; // probably won't change, so likely not needed
+      prevScrollTop.current = listRef.current.scrollTop;
+    };
+    currListRef.addEventListener('scroll', handleScroll);
+
+    return () => {
+      currListRef.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // another option
+  // const componentJustMounted = useRef(true);
+  // useLayoutEffect(() => {
+  //   const currListRef = listRef.current;
+  //   if (!componentJustMounted.current) {
+  //     const lastChild = currListRef.lastChild;
+  //     const lastChildHeight = lastChild.lastChild.offsetHeight;
+
+  //     if (
+  //       currListRef.scrollHeight - (currListRef.scrollTop + currListRef.offsetHeight) <
+  //       lastChildHeight * 3
+  //     ) {
+  //       currListRef.scrollTop = currListRef.scrollHeight - currListRef.offsetHeight;
+  //     }
+  //   } else {
+  //     // scroll to bottom when component is first mounted
+  //     currListRef.scrollTop = currListRef.scrollHeight - currListRef.offsetHeight;
+  //     componentJustMounted.current = false;
+  //   }
+  // }, [filteredList]);
+
   return (
     <div className="BlueView">
       <div className="buttons is-centered">
-        <button className="button">All</button>
-        <button className="button">Even</button>
-        <button className="button">Odd</button>
+        <button className="button" onClick={() => setFilter(null)}>
+          All
+        </button>
+        <button className="button" onClick={() => setFilter('even')}>
+          Even
+        </button>
+        <button className="button" onClick={() => setFilter('odd')}>
+          Odd
+        </button>
       </div>
-      <div className="list-container">
-        {/* <ul>
+      <div className="list-container" ref={listRef}>
+        <ul>
           {filteredList.map((num) => (
             <li key={num}>{num}</li>
           ))}
-        </ul> */}
+        </ul>
       </div>
     </div>
   );
--- src/components-hooks/RedGreenView.js
+++ src/components-hooks/RedGreenView.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useRef, useEffect, useLayoutEffect } from 'react';
 
-function RedGreenView() {
-  return <div></div>;
+const useHeightSnapshot = (viewRef) => {
+  const prevHeight = useRef(null);
+  const componentJustMounted = useRef(true);
+
+  useLayoutEffect(() => {
+    if (!componentJustMounted.current) {
+      const viewHeight = viewRef.current.offsetHeight;
+      const snapshot = prevHeight.current;
+      if (snapshot < viewHeight) {
+        viewRef.current.lastChild.innerText = 'Got Taller';
+      } else if (snapshot > viewHeight) {
+        viewRef.current.lastChild.innerText = 'Got Smaller';
+      } else {
+        viewRef.current.lastChild.innerText = 'Same';
+      }
+    }
+    componentJustMounted.current = false;
+    prevHeight.current = viewRef.current.offsetHeight;
+  });
+};
+
+function RedGreenView({ colorProp: { color } }) {
+  const lastColorRef = useRef(null);
+  const viewRef = useRef(null);
+
+  const height = Math.floor(Math.random() * 200) + 300;
+
+  let changed = true;
+  if (lastColorRef.current === color) {
+    changed = false;
+  }
+
+  useEffect(() => {
+    lastColorRef.current = color;
+  }, [color]);
+
+  useHeightSnapshot(viewRef);
+
+  return (
+    <div className={`RedGreenView bg-${color}`} ref={viewRef} style={{ height }}>
+      <p>{color}</p>
+      <p>{changed ? 'CHANGED' : 'NOT CHANGED'}</p>
+      <p></p>
+    </div>
+  );
 }
 
 export default RedGreenView;
